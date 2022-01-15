d5f0fc566a70f2f97016db4bc5231eeb6f1259d1
leejunejae <65431276+leejunejae@users.noreply.github.com>
2022-01-15 14:15:11 +0900

    Add files via upload
    ~2022-01-15

---
+++ Unreal-Engine/Source/ArenaBattle/ABPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPlayerController.h"
+
+void AABPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+}
